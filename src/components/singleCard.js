a0a406cf8ea61fdef5a9fb241014d1a1dc77010d
Shivam <shivamsawarn2003@gmail.com>
2023-12-23 19:25:04 +0530

    Initialize project using Create React App

---
+++ src/components/singleCard.js
@@ -0,0 +1,20 @@
+import './singleCard.css'
+
+export default function singleCard( { card, handleChoice,flipped,disabled }) {
+
+  const handleClick = () => {
+    if(!disabled){
+      handleChoice(card)
+    }
+   
+  }
+
+  return (
+    <div className="card">
+      <div className={flipped? "flipped":""}>
+        <img className="front" src={card.src} alt="card front" />
+        <img className="back" src="/img/cover.png" onClick={handleClick} alt="cover" />
+      </div>
+    </div>
+  )
+}
